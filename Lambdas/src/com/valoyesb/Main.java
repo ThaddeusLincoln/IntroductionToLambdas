9afd93bfa2e572274236eec9e880595d7364e7e0
ThaddeusLincoln/maven-project <beny1419@yahoo.es>
2017-06-11 22:02:50 +0200

    Lambda executing multiple lines

--- Lambdas/src/com/valoyesb/Main.java
+++ Lambdas/src/com/valoyesb/Main.java
@@ -4,20 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// 1. una manera de hacerlo
-		new Thread(new CodeToRun()).start();
-		
-		// 2. tambien puedo utilizar una Anonymous class
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println("Printing from Runnable from anonymous class");
-			}
-		}).start();
-		
 		// 3. and we can use Lambdas
-		new Thread(() -> System.out.println("Printing from Runnable in a LAMBDA")).start();
+		new Thread(() -> {
+			
+			System.out.println("Printing from Runnable in a LAMBDA");
+			System.out.println("Line 2");
+			System.out.println("Line Drei");
+			
+		}).start();
 
 	}
 
