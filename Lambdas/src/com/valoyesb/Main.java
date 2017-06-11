1a760f006c656bf97a14fb3acfb803411c8bf4d0
ThaddeusLincoln/maven-project <beny1419@yahoo.es>
2017-06-11 21:45:37 +0200

    3 implementations of Runnable interface

--- Lambdas/src/com/valoyesb/Main.java
+++ Lambdas/src/com/valoyesb/Main.java
@@ -5,16 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// 1. una manera de hacerlo
-		// new Thread(new CodeToRun()).start();
+		new Thread(new CodeToRun()).start();
 		
 		// 2. tambien puedo utilizar una Anonymous class
-		/*new Thread(new Runnable() {
+		new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				System.out.println("Printing from Runnable from anonymous class");
 			}
-		}).start();*/
+		}).start();
 		
 		// 3. and we can use Lambdas
 		new Thread(() -> System.out.println("Printing from Runnable in a LAMBDA")).start();
