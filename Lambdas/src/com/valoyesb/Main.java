def280cb7773d05ad13c42941e9d7470755c0bae
ThaddeusLincoln/maven-project <beny1419@yahoo.es>
2017-06-11 21:28:21 +0200

    Initial checkin of Introduction to Lambdas - Java Master Class - Section 17 : Lecture 1

---
+++ Lambdas/src/com/valoyesb/Main.java
@@ -0,0 +1,37 @@
+package com.valoyesb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 1. una manera de hacerlo
+		// new Thread(new CodeToRun()).start();
+		
+		// 2. tambien puedo utilizar una Anonymous class
+		/*new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Printing from Runnable from anonymous class");
+			}
+		}).start();*/
+		
+		// 3. and we can use Lambdas
+		new Thread(() -> System.out.println("Printing from Runnable in a LAMBDA")).start();
+
+	}
+
+}
+
+class CodeToRun implements Runnable{
+
+	@Override
+	public void run() {
+		
+		System.out.println("Printing from Runnable");
+		
+	}
+	
+	
+	
+}
