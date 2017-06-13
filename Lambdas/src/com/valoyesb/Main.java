86e61af567909c66257e9c1e5ebfd696b796749e
ThaddeusLincoln/maven-project <beny1419@yahoo.es>
2017-06-13 02:27:58 +0200

    Lamda with interface which methods returns a value, and the lambda is used with more paraeters in a method

--- Lambdas/src/com/valoyesb/Main.java
+++ Lambdas/src/com/valoyesb/Main.java
@@ -5,6 +5,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * @author Benito
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -49,7 +53,28 @@ public class Main {
 		for(Employee employee : employees){
 			System.out.println(employee.getName());
 		}
+		
+		// -------------------------- SAMPLE 2 -------------------------------------------------
+		// using lambdas with an interface which only method returns a value
+		/*String sillyString = doStringStuff(new UpperConcat() {
+			
+			@Override
+			public String upperAndConcat(String s1, String s2) {
+				return s1.toUpperCase() + s2.toUpperCase();
+			}
+		}, employees.get(0).getName(), employees.get(1).getName());
+		System.out.println(sillyString);*/
+		
+		String sillyString2 = doStringStuff((s1, s2) -> 
+			s1.toUpperCase() + s2.toUpperCase(), employees.get(0).getName(), employees.get(1).getName());
+		System.out.println(sillyString2);
+		
+		
 
+	}
+	
+	public static String doStringStuff(UpperConcat uc, String s1, String s2){
+		return uc.upperAndConcat(s1, s2);
 	}
 
 }
@@ -77,7 +102,16 @@ class Employee {
 	public void setAge(int age) {
 		this.age = age;
 	}
+}
+
+
+/**
+ * @author Benito
+ * La usamos para el ejemplo, ya que contiene un method que devuelve un valor
+ *
+ */
+interface UpperConcat{
 	
-	
+	public String upperAndConcat(String s1, String s2);
 	
 }
