dc5f24c5f404f544de7e16ae5285d05972c4abeb
ThaddeusLincoln/maven-project <beny1419@yahoo.es>
2017-06-18 23:48:54 +0200

    Lambdas vs Anonymous variables scope

--- Lambdas/src/com/valoyesb/Main.java
+++ Lambdas/src/com/valoyesb/Main.java
@@ -65,12 +65,26 @@ public class Main {
 		}, employees.get(0).getName(), employees.get(1).getName());
 		System.out.println(sillyString);*/
 		
-		String sillyString2 = doStringStuff((s1, s2) -> 
+		// -------------------------- SAMPLE 3 -------------------------------------------------
+		/*String sillyString2 = doStringStuff((s1, s2) -> 
 			s1.toUpperCase() + s2.toUpperCase(), employees.get(0).getName(), employees.get(1).getName());
-		System.out.println(sillyString2);
+		System.out.println(sillyString2);*/
 		
+		// -------------------------- SAMPLE 4 -------------------------------------------------
+		/*UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+		String sillyString3 = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+		System.out.println(sillyString3);*/
 		
-
+		// -------------------------- SAMPLE 5 -------------------------------------------------
+		// The anonymous class doesn't has a name, which makes sense
+		AnotherClass anotherClass = new AnotherClass();
+		String s = anotherClass.doSomething();
+		System.out.println(s);
+		
+		// -------------------------- SAMPLE 5 -------------------------------------------------
+		AnotherClass anotherClass2 = new AnotherClass();
+		String s1 = anotherClass2.doSomethingWithLambda();
+		System.out.println(s1);
 	}
 	
 	public static String doStringStuff(UpperConcat uc, String s1, String s2){
@@ -115,3 +129,29 @@ interface UpperConcat{
 	public String upperAndConcat(String s1, String s2);
 	
 }
+
+class AnotherClass{
+	
+	public String doSomething(){
+		System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
+		return Main.doStringStuff(new UpperConcat() {
+			
+			@Override
+			public String upperAndConcat(String s1, String s2) {
+				System.out.println("The anonymous class's name is " + getClass().getSimpleName());
+				return s1.toUpperCase() + s2.toUpperCase();
+			}
+		}, "String1", "String2");
+	}
+	
+	public String doSomethingWithLambda(){
+		System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
+		
+		return Main.doStringStuff((s1, s2) -> 
+			{
+				System.out.println("The AnotherClass class's when calling from the lambda is: " + getClass().getSimpleName());
+				String result1 = s1.toUpperCase() + s2.toUpperCase();
+				return result1;}, "String1", "String2");
+	}
+}
+
